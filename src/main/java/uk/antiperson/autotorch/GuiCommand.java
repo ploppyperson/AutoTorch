8e269f9002534ce9a241a9aced0686123db96c5c
Nathat23 <nathat890@outlook.com>
2024-03-24 20:09:20 +0000

    Only have one menu

--- src/main/java/uk/antiperson/autotorch/GuiCommand.java
+++ src/main/java/uk/antiperson/autotorch/GuiCommand.java
@@ -32,22 +32,8 @@ public class GuiCommand implements CommandExecutor {
         TorchPlacer torchPlacer = autoTorch.getPlacerManager().getTorchPlacer((Player) sender);
         Gui gui = new Gui((Player) sender);
         autoTorch.getGuiManager().watch(gui);
-        GuiPage guiPage = new GuiPage(gui.getPlayer(), 1);
         ConfigGui configGui = new ConfigGui(torchPlacer.getPlayerConfig());
-        gui.addPage(0, guiPage);
-        gui.addPage(1, configGui.createPage(gui.getPlayer()));
-        BooleanGuiItem booleanGuiItem = new BooleanGuiItem();
-        GuiItemStack enabled = new GuiItemStack(new ItemStack(Material.GREEN_WOOL), "Automatic Torch Placing", Arrays.asList(ChatColor.GRAY + "Enabled"));
-        enabled.setOnClick(click -> torchPlacer.setEnabled(false));
-        GuiItemStack disabled = new GuiItemStack(new ItemStack(Material.RED_WOOL), "Automatic Torch Placing", Arrays.asList(ChatColor.GRAY + "Disabled"));
-        disabled.setOnClick(click -> torchPlacer.setEnabled(true));
-        booleanGuiItem.setTrueItem(enabled);
-        booleanGuiItem.setFalseItem(disabled);
-        booleanGuiItem.setDefaultState(torchPlacer.isEnabled());
-        guiPage.putItem(0, booleanGuiItem);
-        ChangePageItem changePageItem = new ChangePageItem(gui, 1);
-        changePageItem.setItemStack(new GuiItemStack(new ItemStack(Material.FURNACE), "Settings", Arrays.asList(ChatColor.WHITE + "Modify torch placing settings")));
-        guiPage.putItem(8, changePageItem);
+        gui.addPage(0, configGui.createPage(gui.getPlayer()));
         gui.show();
         return false;
     }
