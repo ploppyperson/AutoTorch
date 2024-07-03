a753d0be4d6db925d88481ea01ae30b854a6f779
ploppyperson <nathat890@outlook.com>
2024-07-03 10:06:21 +0100

    Add update checker

---
+++ src/main/java/uk/antiperson/autotorch/Updater.java
@@ -0,0 +1,74 @@
+package uk.antiperson.autotorch;
+
+import org.bukkit.plugin.Plugin;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+
+public class Updater {
+
+    private final int resourceId;
+    private final Plugin sm;
+    public Updater(Plugin sm, int resourceId) {
+        this.sm = sm;
+        this.resourceId = resourceId;
+    }
+
+    public CompletableFuture<UpdateResult> checkUpdate() {
+        return CompletableFuture.supplyAsync(() -> {
+            String latestVersion = getLatestVersion();
+            if (latestVersion == null) {
+                return new UpdateResult(VersionResult.ERROR);
+            }
+            String strippedLatest = latestVersion.replaceAll("[^A-Za-z0-9]", "");
+            String strippedCurrent = sm.getDescription().getVersion().replaceAll("[^A-Za-z0-9]", "");
+            if (strippedCurrent.equals(strippedLatest)) {
+                return new UpdateResult(VersionResult.NONE);
+            }
+            return new UpdateResult(VersionResult.AVAILABLE, latestVersion);
+        });
+    }
+
+    private String getLatestVersion(){
+        try{
+            URL updateUrl = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + resourceId);
+            HttpURLConnection connect = (HttpURLConnection) updateUrl.openConnection();
+            connect.setRequestMethod("GET");
+            return new BufferedReader(new InputStreamReader(connect.getInputStream())).readLine();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public enum VersionResult {
+        AVAILABLE,
+        NONE,
+        ERROR
+    }
+
+    public static class UpdateResult {
+
+        private final VersionResult result;
+        private String newVersion;
+        UpdateResult(VersionResult result, String newVersion) {
+            this.result = result;
+            this.newVersion = newVersion;
+        }
+
+        UpdateResult(VersionResult result) {
+            this.result = result;
+        }
+
+        public VersionResult getResult() {
+            return result;
+        }
+
+        public String getNewVersion() {
+            return newVersion;
+        }
+    }
+}
